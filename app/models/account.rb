358a9fabf4683c3383eab37dab3294a00ee159b4
selvaganeshc1 <selvaganesh.c@justintegrateit.com>
2015-05-28 18:52:23 +0530

    Shopify Product Import

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ActiveRecord::Base
-	validates_presence_of :shopify_account_url
+ validates_presence_of :shopify_account_url
 validates_presence_of :shopify_api_key
 
 validates_presence_of :shopify_password
---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    name "MyString"
+shopify_product_id 1
+last_shopify_sync "2015-05-28 18:37:37"
+  end
+
+end
